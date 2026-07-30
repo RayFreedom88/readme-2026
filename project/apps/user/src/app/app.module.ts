30c0c07297e99e2e81e1ac78d6916775b4be2eda
Ildar Musin <imusin@unisender.com>
2026-07-30 19:59:50 +0200

    feat(user): wire Mongoose with async MongoDB config
    Add a connection-string helper and getMongooseOptions so the user app
    can boot Mongoose from ConfigService.

--- project/apps/user/src/app/app.module.ts
+++ project/apps/user/src/app/app.module.ts
@@ -1,11 +1,17 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 
 import { AuthModule } from '@project/auth';
 import { BlogUserModule } from '@project/blog-user';
-import { UserConfigModule } from '@project/user-config';
+import { getMongooseOptions, UserConfigModule } from '@project/user-config';
 
 @Module({
-  imports: [AuthModule, BlogUserModule, UserConfigModule],
+  imports: [
+    AuthModule,
+    BlogUserModule,
+    UserConfigModule,
+    MongooseModule.forRootAsync(getMongooseOptions()),
+  ],
   controllers: [],
   providers: [],
 })
--- project/libs/shared/helpers/src/index.ts
+++ project/libs/shared/helpers/src/index.ts
@@ -1,1 +1,2 @@
 export { fillDto } from './lib/fill-dto';
+export { getMongoConnectionString } from './lib/get-mongo-connection-string';
