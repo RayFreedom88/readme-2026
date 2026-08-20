a533d72ff393c8916149b3b8f7a2982108cb1025
Ildar Musin <imusin@unisender.com>
2026-08-20 17:30:08 +0200

    feat(data-access): add BasePostgresRepository export to index

--- project/libs/shared/data-access/src/index.ts
+++ project/libs/shared/data-access/src/index.ts
@@ -1,3 +1,4 @@
 export { BaseMemoryRepository } from './repository/base-memory.repository';
 export { BaseMongoRepository } from './repository/base-mongo.repository';
+export { BasePostgresRepository } from './repository/base-postgres.repository';
 export type { Repository } from './repository/repository.interface';
